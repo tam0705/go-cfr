5de3fc1c8df389d2e6468a813436fd2a2d62928e
tam_0705 <tamrin.vincent@gmail.com>
2021-12-16 15:24:40 +0800

    Repair

--- tree/helpers.go
+++ tree/helpers.go
@@ -14,14 +14,14 @@ func Visit(root cfr.GameTreeNode, visitor func(node cfr.GameTreeNode)) {
 	root.Close()
 }
 
-func VisitInfoSets(root cfr.GameTreeNode, visitor func(player int, infoSet cfr.InfoSet, n cfr.GameTreeNode)) {
+func VisitInfoSets(root cfr.GameTreeNode, visitor func(n cfr.GameTreeNode, player int, infoSet cfr.InfoSet)) {
 	seen := make(map[string]struct{})
 	Visit(root, func(node cfr.GameTreeNode) {
 		if node.Type() == cfr.PlayerNodeType {
+			n := node
 			player := node.Player()
 			infoSet := node.InfoSet(player)
 			key := infoSet.Key()
-			n := node
 			if _, ok := seen[string(key)]; ok {
 				return
 			}
@@ -51,6 +51,6 @@ func CountNodes(root cfr.GameTreeNode) int {
 
 func CountInfoSets(root cfr.GameTreeNode) int {
 	total := 0
-	VisitInfoSets(root, func(player int, infoSet cfr.InfoSet) { total++ })
+	VisitInfoSets(root, func(node cfr.GameTreeNode, player int, infoSet cfr.InfoSet) { total++ })
 	return total
 }
