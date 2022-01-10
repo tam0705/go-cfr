7174ba5afff6b333dc96152939402e9280def21c
bconstantine <bryan.constantine7@gmail.com>
2022-01-10 14:21:54 +0800

    ratio preflop fix

--- holdem/utility.go
+++ holdem/utility.go
@@ -178,7 +178,7 @@ func GetDecision(Informations Def.RobotInherit, Standard, Total, RaiseDiff, AllI
 	myStrategy = GetStrategy(myHistory)
 	var raisePass float64 = myStrategy[2] * RAISE_REDUCE
 	var allInPass float64 = myStrategy[3] * ALLIN_REDUCE
-	if currentRound == 0 && myHistory[0] > '8' {
+	if currentRound == 0 && myHistory[0] == '9' {
 		myStrategy[0] += myStrategy[1] * PREFLOP_HIGHPASS_CHECK
 		myStrategy[0] += myStrategy[2] * PREFLOP_HIGHPASS_RAISE
 		myStrategy[0] += myStrategy[3] * PREFLOP_HIGHPASS_ALLIN
